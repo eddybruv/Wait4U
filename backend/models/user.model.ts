c18ca3492e64408617a941dca2dab47d1a7eb353
eddybruv <edwinajong@gmail.com>
2022-07-19 20:25:41 +0100

    defined user schema

---
+++ backend/models/user.model.ts
@@ -0,0 +1,26 @@
+import {IUser} from "../types/user.type"
+import {Schema, model} from "mongoose";
+
+const UserSchema: Schema = new Schema(
+  {
+    name: { type: "String", required: true },
+    email: { type: "String", unique: true, required: true },
+    password: { type: "String", required: true },
+    pic: {
+      type: "String",
+      required: true,
+      default:
+        "https://icon-library.com/images/anonymous-avatar-icon/anonymous-avatar-icon-25.jpg",
+    },
+    isAdmin: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export default model<IUser>("user", UserSchema);
