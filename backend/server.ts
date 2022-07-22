7aa8694252af32457f116f0594d44adc85120857
eddybruv <edwinajong@gmail.com>
2022-07-22 20:10:23 +0100

    added chat routes and error handler fxns

--- backend/server.ts
+++ backend/server.ts
@@ -1,20 +1,24 @@
+import { errorHandler, notFound } from "./middleware/errorMiddleware";
 import express from "express";
 import connectDB from "./config/db";
 import chats from "./data/data";
-import UserRouter from "./routes/user.route"
+import UserRouter from "./routes/user.route";
+import ChatRouter from "./routes/chat.route";
 
 require("dotenv").config();
 
 const app = express();
 
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
-app.use('/api/user', UserRouter)
-
+app.use("/api/user", UserRouter);
+app.use("/api/chat", ChatRouter)
+app.use(notFound);
+app.use(errorHandler);
 
 connectDB;
-const PORT = process.env.PORT || 8080
+const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => {
   console.log(`😏[server]: Running on Port ${PORT}`);
