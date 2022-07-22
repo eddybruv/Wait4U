fe0d558697e42a120c9ad95b88ca83a8afacd82f
eddybruv <edwinajong@gmail.com>
2022-07-22 20:14:21 +0100

    added login functionality

--- client/src/components/auth/Login.tsx
+++ client/src/components/auth/Login.tsx
@@ -6,8 +6,11 @@ import {
   Input,
   InputGroup,
   InputRightElement,
+  useToast,
   VStack,
 } from "@chakra-ui/react";
+import axios from "axios";
+import { useHistory } from "react-router-dom";
 
 interface IUser {
   email: string;
@@ -19,8 +22,11 @@ const Login = () => {
     email: "",
     password: "",
   });
+  const toast = useToast();
+  const history = useHistory();
 
   const [show, setShow] = useState(false);
+  const [loading, setLoading] = useState(false);
 
   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
     const { value, name } = e.target;
@@ -35,7 +41,52 @@ const Login = () => {
     setShow((show) => !show);
   };
 
-  const handleSubmit = () => {};
+  const handleSubmit = async () => {
+    setLoading(true);
+    if (!user.email || !user.password) {
+      toast({
+        title: "Please fill all the Feilds",
+        status: "warning",
+        duration: 5000,
+        isClosable: true,
+        position: "bottom",
+      });
+      setLoading(false);
+      return;
+    }
+
+    try {
+      const config = {
+        headers: {
+          "Content-type": "application/json",
+        },
+      };
+      const data = await axios
+        .post("/api/user/login", user, config)
+        .then((data) => data.data);
+
+      toast({
+        title: "Login Successful",
+        status: "success",
+        duration: 5000,
+        isClosable: true,
+        position: "bottom",
+      });
+
+      localStorage.setItem("userInfo", JSON.stringify(data));
+      setLoading(false);
+      history.push("/chats");
+    } catch (error) {
+      toast({
+        title: "Error Occured!",
+        status: "error",
+        duration: 5000,
+        isClosable: true,
+        position: "bottom",
+      });
+      setLoading(false);
+    }
+  };
 
   return (
     <VStack spacing={"5px"}>
@@ -70,6 +121,7 @@ const Login = () => {
         width="100%"
         style={{ marginTop: 15 }}
         onClick={handleSubmit}
+        isLoading={loading}
       >
         Login
       </Button>
