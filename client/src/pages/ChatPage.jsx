3d055bc4ceea8d452daf8809c57b2c1237f0d89d
eddybruv <edwinajong@gmail.com>
2022-07-23 20:09:53 +0100

    started page

--- client/src/pages/ChatPage.jsx
+++ client/src/pages/ChatPage.jsx
@@ -1,15 +1,23 @@
 import React, { useEffect, useState } from "react";
+import { ChatState } from "../context/ChatProvider";
 import axios from "axios";
+import { Box } from "@chakra-ui/react";
+import SideDrawer from "../components/misc/SideDrawer";
+import MyChats from "../components/MyChats";
+import ChatBox from "../components/ChatBox";
 
 const ChatPage = () => {
-  const [chats, setChats] = useState([]);
+  const { user } = ChatState();
 
-
-  return <div>
-    {
-      chats.map(chat => <div>{chat.chatName}</div>)
-    }
-  </div>;
+  return (
+    <div style={{ width: "100%" }}>
+      {user && <SideDrawer />}
+      <Box display="flex" justifyContent="space-between" w="100%" p="10px" h="95.1vh">
+        {user && <MyChats />}
+        {user && <ChatBox />}
+      </Box>
+    </div>
+  );
 };
 
 export default ChatPage;
