2c560101809237670c45c532ea9fe6e0ba8fca8a
eddybruv <edwinajong@gmail.com>
2022-07-24 17:17:55 +0100

    ended at input

--- client/src/components/misc/SideDrawer.tsx
+++ client/src/components/misc/SideDrawer.tsx
@@ -1,42 +1,120 @@
-import { Box, Button, Tooltip, Text, Menu, MenuButton } from "@chakra-ui/react";
-import {} from "@chakra-ui/icons"
-import React, { useState } from "react";
+import {
+  Box,
+  Button,
+  Tooltip,
+  Text,
+  Menu,
+  MenuButton,
+  MenuList,
+  Avatar,
+  MenuItem,
+  MenuDivider,
+  Drawer,
+  useDisclosure,
+  DrawerOverlay,
+  DrawerContent,
+  DrawerHeader,
+  DrawerBody,
+  Input,
+} from "@chakra-ui/react";
+import { BellIcon, ChevronDownIcon } from "@chakra-ui/icons";
+import React, { ChangeEvent, useState } from "react";
+import { ChatState } from "../../context/ChatProvider";
+import { IUser } from "../../types/user.types";
+import ProfileModal from "./ProfileModal";
+import { useHistory } from "react-router-dom";
 
 const SideDrawer = () => {
+  const history = useHistory();
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
   const [search, setSearch] = useState("");
   const [seachResult, setSeachResult] = useState([]);
   const [loading, setLoading] = useState(false);
   const [loadingChat, setLoadingChat] = useState();
 
+  const { user } = ChatState() as IUser;
+
+  const handleLogout = () => {
+    localStorage.removeItem("userInfo");
+    history?.push("/");
+  };
+
   return (
-    <Box
-      display="flex"
-      justifyContent={"space-between"}
-      alignItems="center"
-      bg={"white"}
-      w="100%"
-      p="5px 10px 5px 10px"
-      borderWidth={"5px"}
-    >
-      <Tooltip label="Search Users to chat" hasArrow placement="bottom-end">
-        <Button variant="ghost">
-          <i className="fas fa-search"></i>
-          <Text display={{ base: "none", md: "flex" }} p="10px">
-            Search User
-          </Text>
-        </Button>
-      </Tooltip>
+    <>
+      <Box
+        display="flex"
+        justifyContent={"space-between"}
+        alignItems="center"
+        bg={"white"}
+        w="100%"
+        p="5px 10px 5px 10px"
+        borderWidth={"5px"}
+      >
+        <Tooltip label="Search Users to chat" hasArrow placement="bottom-end">
+          <Button variant="ghost" onClick={onOpen}>
+            <i className="fas fa-search"></i>
+            <Text display={{ base: "none", md: "flex" }} p="10px">
+              Search User
+            </Text>
+          </Button>
+        </Tooltip>
 
-      <Text fontSize={"2xl"} fontFamily={"Work sans"}>Wait 4 U</Text>
+        <Text fontSize={"2xl"} fontFamily={"Work sans"}>
+          Wait 4 U
+        </Text>
 
-      <div>
-        <Menu>
-          <MenuButton p={1}>
-            <BellIcon></BellIcon>
-          </MenuButton>
-        </Menu>
-      </div>
-    </Box>
+        <div>
+          <Menu>
+            <MenuButton p={1}>
+              <BellIcon fontSize="2xl" m={1} />
+            </MenuButton>
+            {/* <MenuList></MenuList> */}
+          </Menu>
+          <Menu>
+            <MenuButton as={Button} rightIcon={<ChevronDownIcon />}>
+              <Avatar
+                size="sm"
+                cursor="pointer"
+                name={user?.data?.name}
+                src={user?.data?.pic}
+              />
+            </MenuButton>
+            <MenuList>
+              <ProfileModal user={user?.data}>
+                <MenuItem>My Profile</MenuItem>
+              </ProfileModal>
+              <MenuDivider />
+              <MenuItem onClick={handleLogout}>Logout</MenuItem>
+            </MenuList>
+          </Menu>
+        </div>
+      </Box>
+
+      <Drawer placement="left" onClose={onClose} isOpen={isOpen}>
+        <DrawerOverlay />
+        <DrawerContent>
+          <DrawerHeader borderBottomWidth="1px"> Search Users</DrawerHeader>
+          <DrawerBody>
+            <Box display="flex" paddingBottom={2}>
+              <Input
+                placeholder="Search by name or email"
+                value={search}
+                onChange={(e: ChangeEvent<HTMLInputElement>) =>
+                  setSearch(e.target.value)
+                }
+                mr={2}
+              />
+              <Button
+              // onClick={handleSearch}
+              >
+                Go
+              </Button>
+            </Box>
+          </DrawerBody>
+        </DrawerContent>
+      </Drawer>
+    </>
   );
 };
 
