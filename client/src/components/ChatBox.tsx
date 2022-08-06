9fe433698444f340333f5d6d48eade798133bdec
eddybruv <edwinajong@gmail.com>
2022-08-06 21:45:52 +0100

    started with chat box

--- client/src/components/ChatBox.tsx
+++ client/src/components/ChatBox.tsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from "react";
+import { ChatState } from "../context/ChatProvider";
+import { IUser } from "../types/user.types";
+import { Box } from "@chakra-ui/react";
 
 const ChatBox = () => {
+  const { selectedChat } = ChatState() as any;
   return (
-    <div>ChatBox</div>
-  )
-}
+    <div>
+      <Box display={{ base: selectedChat ? "flex" : "none", md: "flex" }}
+        alignItems="center"
+        flexDirection={"column"}
+        p={3}
+        bg="white"
+        width={{base: "100%", md:"flex"}}
+        borderRadius="lg"
+      >
+        Single Chat
+      </Box>
+    </div>
+  );
+};
 
-export default ChatBox
+export default ChatBox;
