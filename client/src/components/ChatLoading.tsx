f3375723eec6279a9772f0865b633e5ed4aa7051
eddybruv <edwinajong@gmail.com>
2022-08-06 21:45:37 +0100

    loading component

---
+++ client/src/components/ChatLoading.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Stack } from "@chakra-ui/react";
+import { Skeleton } from "@chakra-ui/react";
+
+const ChatLoading = () => {
+  return (
+    <Stack>
+      <Skeleton height="45px" />
+      <Skeleton height="45px" />
+      <Skeleton height="45px" />
+      <Skeleton height="45px" />
+      <Skeleton height="45px" />
+    </Stack>
+  );
+};
+
+export default ChatLoading;
