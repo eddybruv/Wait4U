845e1687c7c37a854b54e9218c9555422faaf209
eddybruv <edwinajong@gmail.com>
2022-09-01 18:25:14 +0100

    continued with markup

--- client/src/components/ChatBox.tsx
+++ client/src/components/ChatBox.tsx
@@ -5,15 +5,17 @@ import { Box } from "@chakra-ui/react";
 
 const ChatBox = () => {
   const { selectedChat } = ChatState() as any;
+  console.log(selectedChat)
   return (
     <div>
-      <Box display={{ base: selectedChat ? "flex" : "none", md: "flex" }}
+      <Box display={{ base: selectedChat.length ? "flex" : "none", md: "flex" }}
         alignItems="center"
         flexDirection={"column"}
         p={3}
         bg="white"
-        width={{base: "100%", md:"flex"}}
+        width={{base: "100%", md:"68%"}}
         borderRadius="lg"
+        borderWidth={"1px"}
       >
         Single Chat
       </Box>
@@ -21,4 +23,4 @@ const ChatBox = () => {
   );
 };
 
-export default ChatBox;
+export default ChatBox
